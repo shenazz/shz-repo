126d6e55df684d22cf545b7c37c7ce5dc65aae39
shenazz <shenaz.assu@gmail.com>
2018-04-05 01:08:13 +0200

    new project for m101j

---
+++ mongodbm101j/src/main/java/com/mongodb/m101j/util/Tester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mongodb.m101j.util;
+
+/**
+ * @author ShenazAssu
+ *
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
