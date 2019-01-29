308b5a8f18423ccb71da5f13b201777ff336246e
Shenaz Assu <shenaz@gmail.com>
2019-01-29 18:35:05 +0100

    core java lab

---
+++ corejava-lab/src/main/java/com/shz/MyLab.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.shz;
+
+import java.util.Scanner;
+
+/**
+ * @author shenaz
+ *
+ */
+public class MyLab {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter your name: ");
+
+		System.out.println("Your name is " + scanner.next());
+		
+		System.out.println("Please enter your age: ");
+
+		System.out.println("Your age is " + scanner.next());
+	}
+
+}
