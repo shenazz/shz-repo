2ef59014ecda6780dbfe92a07896ba30a8f4e8fc
shenazz <shenaz.assu@gmail.com>
2019-10-28 09:46:03 +0100

    html parser

--- shz-service/src/main/java/com/shz/starter/controller/TestController.java
+++ shz-service/src/main/java/com/shz/starter/controller/TestController.java
@@ -3,12 +3,20 @@
  */
 package com.shz.starter.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,16 +34,33 @@ import com.shz.starter.repo.IMotorBikeRepo;
 @RestController
 public class TestController {
 
-	@Autowired
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	IMotorBikeRepo motorBikeRepo;
 
 	private final AtomicLong counter = new AtomicLong();
 
 	/**
 	 * @return
+	 * @throws IOException
+	 */
+	@GetMapping(path = "/parsehtml")
+	public ResponseEntity<String> parseHtml(@RequestParam String url) throws IOException {
+		Document document = Jsoup.connect(url).get();
+		Elements elements = document.select("h5.n-product-docs__tile__title");
+		Element elemnt = elements.first();
+		elemnt.data();
+		String source = document.html();
+		logger.error(source);
+		return new ResponseEntity<>(source, HttpStatus.OK);
+	}
+
+	/**
+	 * @return
 	 */
 	@RequestMapping(path = "/testme", method = RequestMethod.GET)
-	public ResponseEntity<TestMessage> testMe(@RequestParam(value = "msg", defaultValue = "Hello World") String message) {
+	public ResponseEntity<TestMessage> testMe(
+			@RequestParam(value = "msg", defaultValue = "Hello World") String message) {
 		return new ResponseEntity<>(new TestMessage(counter.incrementAndGet(), message), HttpStatus.OK);
 	}
 
@@ -44,9 +69,7 @@ public class TestController {
 	 */
 	@RequestMapping(path = "/play", method = RequestMethod.GET)
 	public ResponseEntity<String> play() {
-		
-		
-		
+
 		return new ResponseEntity<>("Alles gut!", HttpStatus.OK);
 	}
 
