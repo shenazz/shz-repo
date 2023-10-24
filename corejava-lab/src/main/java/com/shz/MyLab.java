82685ae159ed686a162c8bd055cc409f5b5b1d05
Shenaz A <shenaz@sds-systems.com>
2023-10-24 11:23:51 +0200

    let's go!

--- corejava-lab/src/main/java/com/shz/MyLab.java
+++ corejava-lab/src/main/java/com/shz/MyLab.java
@@ -3,7 +3,15 @@
  */
 package com.shz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlContext;
@@ -19,9 +27,40 @@ public class MyLab {
 
 	/**
 	 * @param args
+	 * @throws ExecutionException
+	 * @throws InterruptedException
 	 */
-	public static void main(String[] args) {
-		JexlTest();
+	public static void main(String[] args) throws Exception {
+		//JexlTest();
+
+		streamTest();
+
+	}
+
+	public static void streamTest() throws InterruptedException, ExecutionException {
+		List<Integer> intList = new ArrayList<>();
+		Random random = new Random();
+		for (int i = 0; i < 1000; i++) {
+			intList.add(random.nextInt());
+		}
+
+		ExecutorService pool = Executors.newFixedThreadPool(2);
+
+		Future<?> task2 = pool.submit(() -> {
+			for (int i = 0; i < 1000; i++) {
+				intList.add(random.nextInt());
+				System.out.println("Adding an element");
+			}
+			System.out.println("Completed 2");
+		});
+
+		intList.stream().map(element -> {
+			System.out.println("Mapping an element");
+			return element.toString();
+		}).collect(Collectors.toList());
+
+		task2.get();
+
 	}
 
 	public static void varArgTest(String... names) {
